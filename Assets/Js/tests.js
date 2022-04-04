ef9573374385ac7f319cb52a83339551513e5f88
BrotherBoris <joaoantoniofer@gmail.com>
2022-04-04 18:47:22 -0300

    minor chaanges 04-04-22

--- Assets/Js/tests.js
+++ Assets/Js/tests.js
@@ -11,3 +11,29 @@ function updateTime() {
         }
     }
 }
+
+let stop = 5;
+function Looper() {
+    setTimeout(myFunction, 1000);
+}
+
+function myFunction() {
+    if(stop != 0){
+        stop--;
+        const d = new Date;
+        console.log(d.getSeconds());
+        setTimeout(myFunction, 1000);
+    }
+    if (stop < 0) {
+        stop = 5;
+    }
+}
+
+
+/* const {late} = await fetchBill();
+
+if (late > 0) {
+    let faded = (100-late)/100;
+    document.body.style.opacity = faded;
+} */
+//document.body.style.opacity = 0.25;
