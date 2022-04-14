a0c6449ca520828d72ebbce358c05a97c137cf1e
BrotherBoris <joaoantoniofer@gmail.com>
2022-04-14 01:29:52 -0300

    some progress 14-04-22

---
+++ Assets/Js/dataEstructure.js
@@ -0,0 +1,12 @@
+
+let array = [];
+
+
+function addElement(element) {
+    array.push(element);
+
+}
+
+function removeElement() {
+    array.pop();
+}
--- Assets/Js/tests.js
+++ Assets/Js/tests.js
@@ -1,13 +1,41 @@
 
 
-let array = [];
-console.log(array);
+class Card {
+    constructor(name, maxHealth, maxDefense, maxAttack){
+        this.name = name;
+        this.maxHealth = maxHealth;
+        this.currentHealth = maxHealth;
+        this.maxDefense = maxDefense;
+        this.currentDefense = maxDefense;
+        this.maxAttack = maxAttack;
+        this.currentAttack = maxAttack;
+    }
+    toString(){
+        return "name:["+this.name+"]health:["+this.currentHealth+"/"+this.maxHealth+"]"+"defense:["+this.currentDefense+"/"+this.maxDefense+"]"+"attack:["+this.currentAttack+"/"+this.maxAttack+"]";
+    }
 
-function addElement(element){
-    array.push(element);
-    
+}
+newCardo = new Card("cardo",2);
+
+function creatNewCard() {
+    let newCard = new Card("jose",5);
+    console.log(newCard.toString());
+
 }
 
-function removeElement(){
-    array.pop();
+let cardArray = [];
+
+function dumpCards(array) {
+    array.push(peasant = new Card("Peasant", 2, 1, 2));
+    array.push(swrdKng = new Card("Sword Knight", 3, 2, 3));
+    array.push(turtleKng = new Card("Turtle Knight", 4, 7, 2));
+    array.push(dragon = new Card("Dragon", 5, 5, 6));
+}
+
+dumpCards(cardArray);
+
+
+for (let i = 0; i < cardArray.length; i++) {
+    console.log(cardArray[i].toString());
+    
 }
